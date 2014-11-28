4222f50f36ce274b427f6ceddc679055fabfb0e3
minicl55 <whatsittoya573@gmail.com>
2014-11-28 16:52:04 -0500

    Added GET paramater to userPosts.php

--- api/userPosts.php
+++ api/userPosts.php
@@ -1,15 +1,26 @@
 <?php
-    require ('/var/www/civ/other/req.php');
-    $query = "SELECT poster,COUNT(`poster`) AS totalPosts FROM offers GROUP BY poster ORDER BY totalPosts DESC";
-    $result = mysqli_stmt_init($con);
-    $result->prepare($query);
-    $result->execute();
-    $result = $result->get_result();
-    $r = array();
-    $index = 0;
-    while ($row = mysqli_fetch_assoc($result)) {
-        $rTemp = array("name" => $row['poster'], "amount" => $row['totalPosts']);
-        array_push($r, $rTemp);
+    require ('/var/www/civbeta/other/req.php');
+    if (!isset($_GET['user'])) {
+        $query = "SELECT poster,COUNT(`poster`) AS totalPosts FROM offers GROUP BY poster ORDER BY totalPosts DESC";
+        $result = mysqli_stmt_init($con);
+        $result->prepare($query);
+        $result->execute();
+        $result = $result->get_result();
+        $r = array();
+        $index = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $rTemp = array("name" => $row['poster'], "amount" => $row['totalPosts']);
+            array_push($r, $rTemp);
+        }
+        echo json_encode($r);
     }
-    echo json_encode($r);
+    else {
+        $query = "SELECT COUNT(`poster`) AS posts FROM offers WHERE poster = ? GROUP BY poster DESC";
+        $stmt = mysqli_stmt_init($con);
+        $stmt->prepare($query);
+        $stmt->bind_param('s', $_GET['user']);
+        $stmt->execute();
+        $result = mysqli_fetch_row($stmt->get_result());
+        echo $result[0];
+    }
 ?>
